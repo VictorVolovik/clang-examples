bcdd515a9b0a41b0a6dd870b94567e4dabe391f5
Victor Volovik <andhope2die@gmail.com>
2017-07-15 22:20:50 +0300

    Add King Ch2 basic example

---
+++ King/Ch_002/pun.c
@@ -0,0 +1,12 @@
+/*
+ * Name: pun.c
+ * Purpose: prints a bad pun
+ * Author: me
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  printf("To C, or not to C: that is the question.\n");
+  return 0;
+}
