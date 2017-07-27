a691f05c723521e996940b076e44dcc510a18f79
Victor Volovik <andhope2die@gmail.com>
2017-07-27 17:12:05 +0300

    Add completed exercises for K Ch3

---
+++ King/Ch_003/ex_002.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  float x;
+
+  printf("Enter a floating-point number: ");
+  scanf("%f", &x);
+
+  printf("%-8.1e\n", x);
+  printf("%10.6e\n", x);
+  printf("%-8.3f\n", x);
+  printf("%6.0g\n", x);
+
+  return 0;
+}
